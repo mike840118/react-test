c9d4f5fbf1bb8c9a0dc536f0f5395125fe3988d9
mike840118 <milk840118@yahoo.com.tw>
2025-03-19 09:13:46 +0800

    撰寫menu製作簡單的組件 撰寫假資料 方便使用

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,5 +1,46 @@
-import React, { useState, useEffect } from 'react';
-function menu (){
-    return <div>menu</div>
-    }
-    export default menu
+import React, { useState, useEffect } from "react";
+import { Menu } from "antd";
+import menuData from "./menu.json";
+
+const { SubMenu, ItemGroup, Item } = Menu;
+
+const MenuComponent: React.FC = () => {
+  const [current, setCurrent] = useState<string>("1");
+
+  const handleClick = (e: any) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+  const renderMenuItems = (items: any[]) =>
+    items.map((item) => {
+      if (item.children) {
+        if (item.type === "group") {
+          return (
+            <ItemGroup key={item.key} title={item.title}>
+              {renderMenuItems(item.children)}
+            </ItemGroup>
+          );
+        }
+        return (
+          <SubMenu key={item.key} title={<span>{item.title}</span>}>
+            {renderMenuItems(item.children)}
+          </SubMenu>
+        );
+      }
+      return <Item key={item.key}>{item.title}</Item>;
+    });
+
+  return (
+    <Menu
+      onClick={handleClick}
+      style={{ width: 240, height: "100vh" }}
+      defaultOpenKeys={["sub1"]}
+      selectedKeys={[current]}
+      mode="inline"
+    >
+      {renderMenuItems(menuData)}
+    </Menu>
+  );
+};
+
+export default MenuComponent;
--- src/data/app.tsx
+++ src/data/app.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Menu from '../components/menu.tsx'
 function AppData (){
-return <div>App</div>
+return <><div style={{display:'flex'}}><Menu></Menu>App</div></>
 }
 export default AppData
